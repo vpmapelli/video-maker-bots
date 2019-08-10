381d957d773cf15b91a3a03ae5f164ebf2750f11
vinicius <vinicius@vinicius-debian>
2019-08-10 10:38:38 -0300

    feat: add input and state robot

--- robots/text.js
+++ robots/text.js
@@ -11,13 +11,19 @@ const nlu = new NaturalLanguageUnderstandingV1({
   url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
 })
 
+const state = require('./state.js')
 
-async function robot(content) {
+
+async function robot() {
+ const content = state.load()
+
  await fetchContentFromWikipedia(content)
  sanitizeContent(content)
  breakContentIntoSentences(content)
  limitMaximumSentences(content)
  await fetchKeywordsOfAllSentences(content)
+
+ state.save(content)
 
  async function fetchContentFromWikipedia(content) {
   const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
